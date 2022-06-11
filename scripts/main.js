1d85f35a01ebef2f97244529ffe7c7ab952df540
Lisa Ngo <lisa.t.ngo@gmail.com>
2022-06-11 13:27:51 -0400

    Add files via upload
    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ Hooks.on("init", () => {
 //Neaten out the areas of movement
 //Make Terrain Matter?
 //Diagonal/Resolving Movement etc
+//Add functionality for technique-specific opportunities, merge with Attack-specific opportunity
 let dragRulerZeroBasedMovement = false;
 let dragRulerTacticalBasedMovement = false;
 Hooks.on("ready", () => {
@@ -50,6 +51,10 @@ Hooks.once("canvasInit", () => {
         }
     }
 })
+Hooks.once('ready',() =>{
+    dice_helper();
+    create_and_populate_journal();
+})
 Hooks.once("dragRuler.ready", (SpeedProvider) => {
     class LegendOfTheFiveRingsSpeedProvider extends SpeedProvider {
         get colors(){
@@ -80,12 +85,175 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
     }
     dragRuler.registerModule("l5r-dragruler", LegendOfTheFiveRingsSpeedProvider)
 })
-function GetRangeDefault(){
-    var setZero = (dragRulerZeroBasedMovement)? 1 : 0;
-    var defaultRange = new Array(1-setZero,2-setZero,3-setZero,6-setZero,10-setZero,15-setZero,20-setZero,21-setZero);
-    var tacticalRange = new Array(0,2,4,10,100,101,102);
-    return (dragRulerTacticalBasedMovement)? tacticalRange : defaultRange;
+Handlebars.registerHelper("times", function (times, opts) {
+    var out = "";
+    var i;
+    var data = {};
+
+    if ( times ) {
+        for ( i = 0; i < times; i += 1 ) {
+            data.index = i;
+            out += opts.fn(this, {
+                data: data
+            });
+        }
+    } else {
+
+        out = opts.inverse(this);
+    }
+
+    return out;
+});
+
+async function socket_listener(data){
+    if(data.type === "dice"){
+        if(game.user.isGM) {
+            dice_helper_clicked(data.object);
+        }
+    }
 }
+
+async function dice_helper_clicked(object){
+    if(!game.user.isGM){
+        game.socket.emit(
+            'l5r-dragruler',
+            {
+                type: 'dice',
+                object:object
+            }
+        );
+        return;
+    }
+    var data = determine_data(object.message.content);
+
+    let skill = data['skill'];
+    let suggestions = await fetch_suggestions(data);
+
+    var msg = new ChatMessage(object.message);
+    let context = {
+        suggestions: suggestions.suggestions,
+        image: suggestions.image,
+        acting: suggestions.description[0].acting,
+        description: suggestions.description[0].descriptor,
+        skill: skill.capitalize(),
+        stance: suggestions.category
+    };
+    object.message.content = (await getTemplate('modules/l5r-dragruler/templates/dice_helper.html'))(context);
+    object.message.id = object.message._id;
+    msg.update(object.message);
+}
+
+async function fetch_suggestions(results) {
+    let skillGroup = (results['roll']!=undefined)? results['roll']['l5r5e'].skillCatId : results['skillGroup'] ;
+    let skill = (results['roll']!=undefined)? results['roll']['l5r5e'].skillId : results['skill'] ;
+    let category = (results['roll']!=undefined)? results['roll']['l5r5e'].stance : results['stance'];
+    let suggestions = [];
+    let skillSuggestions = [];
+    let data = load_data();
+    var initiativeSkills = ["sentiment", "meditation","tactics", "command"];
+    var attackSkills = ["ranged","melee","unarmed"];
+    var categoryImagePath = "systems/l5r5e/assets/icons/rings/"+category+".svg";
+
+    category = category.capitalize();
+    //get base
+    let tmpSuggestions = data[category].base.filter(suggestion => suggestion.required <= results["opportunity"]);
+    //get skillgroup opps
+    if(skillGroup == "martial"){
+        skillSuggestions = data[category].conflict.filter(suggestions => suggestions.required <= results["opportunity"]);
+        if(attackSkills.includes(skill) && results['success']>=2){
+            var sk = {
+                "text":"Inflict a critical strike on your target with severity equal to your weapon's deadliness.", 
+                "required": 2,
+                "extra": false,
+                "action": "strike"
+            }
+            skillSuggestions.push(sk);
+        }
+    } else if(skillGroup != "") {
+        skillSuggestions.push(data[category]["skills"][skillGroup]);
+    }
+    //initiative
+    if(initiativeSkills.includes(skill)){
+        var sk = data[category]["skills"].initiative;
+        skillSuggestions.push(sk);
+    }
+    suggestions = {
+        suggestions: tmpSuggestions.concat(skillSuggestions),
+        description: data[category].desc,
+        image: categoryImagePath,
+        category: category
+    };
+    return suggestions;
+}
+
+export function dice_helper(){
+    game.socket.on("l5r-dragruler", socket_listener);
+    Hooks.on("createChatMessage", (messageData, meta_data, id) =>{
+        if(game.settings.get("l5r-dragruler", "opportunity-helper")){
+            if(is_roll(messageData) === true && messageData["_roll"]!= null 
+                && messageData["_roll"]["l5r5e"]["history"]!=null // Reveal on second roll
+                && messageData["roll"]["l5r5e"]["summary"].opportunity > 0){ 
+                let skillGroup = messageData["roll"]["l5r5e"].skillCatId;
+                let skill = messageData["roll"]["l5r5e"].skillId;
+                let stance = messageData["roll"]["l5r5e"].stance;
+                let opp = messageData["roll"]["l5r5e"]["summary"].opportunity;
+                let succ = messageData["roll"]["l5r5e"]["summary"].totalSuccess;
+                
+                var msg = {
+                    type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+                    'content': '<button class="l5rSpend" '+
+                               'data-st="' + stance + '" ' +
+                               'data-skG="' + skillGroup + '" '+
+                               'data-sk="' + skill + '" '+
+                               'data-op="' + opp + '" ' + 
+                               'data-succ="' + succ + '" ' + 
+                               '> Help Spend Results?</button>'
+                };
+                ChatMessage.create(msg);
+            }
+        }
+
+    });
+    Hooks.on("renderChatMessage", (app, html, messageData) => {
+        if (game.settings.get("l5r-dragruler", "opportunity-helper")) {
+            // this would need to remain in renderchatmessage since we don't have easy access to the HTML later
+            html.on("click", ".l5rSpend", async function () {
+                await dice_helper_clicked(messageData);
+            });
+        }
+    })
+}
+
+export async function create_and_populate_journal(){
+    if (!game.settings.get("l5r-dragruler", "opportunity-helper")) {
+        return;
+    }
+
+    // otherwise check to see if the journal already exists
+    let journal_name = game.settings.get("l5r-dragruler", "opportunity-helper-data");
+    let journal = game.journal.filter(journal => journal.name === journal_name);
+
+    if (journal.length === 0) {
+        // journal doesn't exist
+
+        // let's search for a translated one (will probably show an error in console, can't avoid it)
+        let jsonFilePath = "modules/l5r-dragruler/content/op_helper" + game.i18n.lang + ".json"
+        await fetch(jsonFilePath).then(response => {
+            if(!response.ok) {
+                jsonFilePath = "modules/l5r-dragruler/content/op_helper.json";
+            }
+        });
+
+        // then create journal
+        let suggestions = await $.getJSON(jsonFilePath);
+        let data = {
+            "name": journal_name,
+            "content": JSON.stringify(suggestions),
+        };
+        JournalEntry.create(data);
+    }
+}
+
 function registerSettings() {
     game.settings.register("l5r-dragruler", "dragRulerSupport", {
         name: "l5r-dragruler.settings.dragRulerSupport.name",
@@ -109,9 +277,84 @@ function registerSettings() {
 		type: Boolean,
 		default: false,
     })
+    game.settings.register("l5r-dragruler","opportunity-helper",{
+        name: "Opportunity Assistance.",
+        hint: "Enable opportunity roll assistance.",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true
+    })
+    game.settings.register("l5r-dragruler","opportunity-helper-data",{
+        name: "Dice Helper Journal",
+        hint: "Dice helper suggestions will be read from this journal.",
+        scope: "world",
+        config: true,
+        type: String,
+        default: "dice_helper"
+    })
 }
+function GetRangeDefault(){
+    var setZero = (dragRulerZeroBasedMovement)? 1 : 0;
+    var defaultRange = new Array(1-setZero,2-setZero,3-setZero,6-setZero,10-setZero,15-setZero,20-setZero,21-setZero);
+    var tacticalRange = new Array(0,2,4,10,100,101,102);
+    return (dragRulerTacticalBasedMovement)? tacticalRange : defaultRange;
+}
+
+function load_data(){
+    let journal_name = game.settings.get("l5r-dragruler", "opportunity-helper-data");
+    let journal = game.journal.filter(journal => journal.name === journal_name);
+
+    if (journal.length <= 0) {
+        ui.notification.warn("Failed to find journal - make sure it's created or something");
+        return {};
+    }
+    try {
+        let data = journal[0].data.content.replace('<p>', '').replace('</p>', '');
+        let jsondata = JSON.parse(data.replace('\"', '"'));
+        // Let translate the skill names if possible
+        Object.keys(jsondata).forEach( skillname => {
+            if(skillname.includes("SWFFG.")){
+                let localizedskill = game.i18n.localize(skillname).toLowerCase();
+                Object.defineProperty(jsondata, localizedskill,
+                    Object.getOwnPropertyDescriptor(jsondata, skillname));
+                delete jsondata[skillname];
+            }
+        });
+        return jsondata;
+    } catch(err) {
+        ui.notification.warn("Dice helper: invalid data detected in journal");
+        return {};
+    }
+}
+
+function determine_data(incoming_data) {
+    /**
+     * read the button metadata to determine results from the associated dice roll
+     *
+     * @param {incoming_data} html created by dice_helper
+     */
+    let data = $(incoming_data);
+    return {
+        'opportunity': data.data('op'),
+        'stance': data.data('st'),
+        'skillGroup': data.data('skg'),
+        'skill': data.data("sk"),
+        'success': data.data("succ")
+    };
+}
+
 function getAllPreviousRayWidths() {
 
+}
+function is_roll(message_data) {
+    if (game.user.isGM && message_data['_roll'] !== null) {
+        if (message_data['data']['roll'] === undefined) {
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
 function changeLabelNames(text) {
     let returnedText = "";
